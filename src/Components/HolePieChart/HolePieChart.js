76d24ba0473ed9a98441461b17c067900280e5d1
Cody Evins <cody.evins@gmail.com>
2020-12-12 15:20:41 -0500

    Pie Chart numbers moved to outside

--- src/Components/HolePieChart/HolePieChart.js
+++ src/Components/HolePieChart/HolePieChart.js
@@ -20,7 +20,7 @@ const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
   cx, cy, midAngle, innerRadius, outerRadius, percent, index,
 }) => {
-   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+   const radius = innerRadius + (outerRadius - innerRadius) * 1.1;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
