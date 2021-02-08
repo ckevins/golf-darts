9f2dcb685e6191b0532a49fb82fa3ac3d51b4ebd
Cody Evins <cody.evins@gmail.com>
2021-02-08 14:05:13 -0500

    Scores GET Handler written

--- api/scores.js
+++ api/scores.js
@@ -1,5 +1,21 @@
-const scoresRouter = require('express').Router();
+const scoresRouter = require('express').Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite');
+
+//select all scores
+scoresRouter.get('/', (req, res, next) => {
+    const sql = `SELECT * FROM Scores WHERE player_id = $player_id`;
+    const values = { $player_id: req.params.player_id };
+    db.all(sql, values, (error, scores) => {
+        if(error){
+            next(error);
+        } else {
+            res.status(200).json({ scores: scores });
+        }
+    })
+});
+
+//select scores by game
+scoresRouter.get('/games')
 
 module.exports = scoresRouter;
